3b65a4d66ea6a36d7f4661d2bf86e7ad07593cd3
melihcskn <melihcskn60@gmail.com>
2024-07-09 02:04:00 +0300

    Dockerize
    Docker files for frontend

--- frontend/movie-site/vite.config.js
+++ frontend/movie-site/vite.config.js
@@ -5,6 +5,15 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
